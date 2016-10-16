5c3e12259f543bc40c37c838a85ba2eef8fd78f3
Abhi Agarwal <me@abhiagarwal.com>
2016-10-16 19:10:11 -0400

    Small changes in layout

--- public/js/newsai.js
+++ public/js/newsai.js
@@ -21,3 +21,27 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
     )
     document.querySelector('head').appendChild(msViewportStyle)
 }
+
+function changePricingClass() {
+    if (document.getElementById("pricingClass").className === "annually") {
+        // Change label color & the button
+        document.getElementById("pricingClass").className = "monthly";
+        document.getElementById("monthlyLabel").className = "active";
+        document.getElementById("annuallyLabel").className = "";
+
+        // Update prices
+        document.getElementById("personalPrice").innerHTML = "9.99";
+        document.getElementById("businessPrice").innerHTML = "49.99";
+        document.getElementById("ultimatePrice").innerHTML = "79.99";
+    } else {
+        // Change label color & the button
+        document.getElementById("pricingClass").className = "annually";
+        document.getElementById("monthlyLabel").className = "";
+        document.getElementById("annuallyLabel").className = "active";
+
+        // Update prices
+        document.getElementById("personalPrice").innerHTML = "7.99";
+        document.getElementById("businessPrice").innerHTML = "39.99";
+        document.getElementById("ultimatePrice").innerHTML = "63.99";
+    }
+}
